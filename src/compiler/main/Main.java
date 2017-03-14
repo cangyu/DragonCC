01c5f5806046b20db03fb3fe714e62406359f114
Yu Cang <yhcy1993@gmail.com>
2017-03-14 22:39:31 +0800

    reform to the entrance of the project

--- src/compiler/main/Main.java
+++ src/compiler/main/Main.java
@@ -1,22 +1,36 @@
 package compiler.main;
 
+import java.io.*;
 import java.util.*;
+import org.json.*;
+import com.google.gson.*;
+import java_cup.runtime.*;
+import compiler.syntactic.*;
+import compiler.semantic.*;
 
 public class Main 
-{
-	public static void main(String[] args) 
+{    
+	public static void main(String[] args) throws Exception
 	{
-	    //test iterator
-	    LinkedList<Integer> ta = new LinkedList<Integer>();
-	    
-	    for(int i=0;i<10;i++)
-	        ta.add(i);
-	    
-	    Iterator<Integer> it = ta.iterator();
-	    
-	    while(it.hasNext())
-	    {
-	        System.out.println(it.next());
-	    }
+        InputStream inp = new FileInputStream("tests/example.c");
+        Parser parser = new Parser(inp);
+        java_cup.runtime.Symbol parseTree = null;
+
+        try
+        {
+            parseTree = parser.parse();
+        }
+        catch (Throwable e)
+        {
+            e.printStackTrace();
+            throw new Error(e.toString());
+        }
+        finally
+        {
+            inp.close();
+        }
+
+        Semantic sc = new Semantic(parseTree.value);
+        sc.checkProgram();
 	}
 }
