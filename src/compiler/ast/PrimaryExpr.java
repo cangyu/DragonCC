032eacb85f2ffcd7fc6a85c274c4933957859621
Yu Cang <yhcy1993@gmail.com>
2017-03-17 21:34:11 +0800

    auto formated

--- src/compiler/ast/PrimaryExpr.java
+++ src/compiler/ast/PrimaryExpr.java
@@ -2,17 +2,20 @@ package compiler.ast;
 
 public class PrimaryExpr extends Expr
 {
-	public static enum ElemType{ID, STRING, CHAR, INT};
-	
-	public ElemType elem_type;
-	public Object elem;
-	
-	public PrimaryExpr(ElemType _t, Object _e)
-	{
-		elem_type = _t;
-		elem = _e;
-	}
-	
+    public static enum ElemType
+    {
+        ID, STRING, CHAR, INT
+    };
+
+    public ElemType elem_type;
+    public Object elem;
+
+    public PrimaryExpr(ElemType _t, Object _e)
+    {
+        elem_type = _t;
+        elem = _e;
+    }
+
     public void accept(ASTNodeVisitor v)
     {
         v.visit(this);
