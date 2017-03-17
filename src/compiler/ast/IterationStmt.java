76121784deb8956968d1beb8f6cb24cb3ed57f8d
Yu Cang <yhcy1993@gmail.com>
2017-03-17 21:21:08 +0800

    rename variables

--- src/compiler/ast/IterationStmt.java
+++ src/compiler/ast/IterationStmt.java
@@ -5,23 +5,22 @@ public class IterationStmt extends Stmt
 	public static enum Type{WHILE, FOR};
 	
 	public Type iteration_type;
-	public Expr cond;
+	public Expr init, judge, next;
 	public Stmt stmt;
-	public Expr init_op, next_op;
 	
 	public IterationStmt(Expr _cond, Stmt _s)
 	{
 		iteration_type = Type.WHILE;
-		cond = _cond;
+		judge = _cond;
 		stmt = _s;
 	}
 	
 	public IterationStmt(Expr _t1, Expr _t2, Expr _t3, Stmt _s)
 	{
 		iteration_type = Type.FOR;
-		init_op = _t1;
-		cond = _t2;
-		next_op = _t3;
+		init = _t1;
+		judge = _t2;
+		next = _t3;
 		stmt = _s;
 	}
 	
