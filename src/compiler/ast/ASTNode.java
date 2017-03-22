b57699636dd673f4252e17e11739398d592258c7
CangYu <yhcy1993@gmail.com>
2017-03-22 10:39:58 +0800

    add toString

--- src/compiler/ast/ASTNode.java
+++ src/compiler/ast/ASTNode.java
@@ -5,4 +5,21 @@ public abstract class ASTNode
 	public String [] ast_rep;
 	
     abstract void accept(ASTNodeVisitor v);
+    
+    public String toString()
+    {
+    	String ans="";
+    	
+    	if(ast_rep!=null)
+    	{
+    		int n = ast_rep.length;
+    		for(int i=0;i<n;i++)
+    		{
+    			ans+=ast_rep[i];
+    			ans+="\n";
+    		}
+    	}
+    	
+    	return ans;
+    }
 }
