d8b51cf5c876e9f1c3009dc8ee5106ce6a000869
CangYu <yhcy1993@gmail.com>
2017-03-22 18:55:37 +0800

    param rename

--- src/compiler/ast/ASTNodeVisitor.java
+++ src/compiler/ast/ASTNodeVisitor.java
@@ -3,47 +3,47 @@ package compiler.ast;
 public interface ASTNodeVisitor
 {
     /* Expr */
-    public void visit(Expression exp);
-    public void visit(AssignmentExpr ae);
-    public void visit(BinaryExpr be);
-    public void visit(CastExpr ce);
-    public void visit(UnaryExpr ue);
-    public void visit(PostfixExpr pe);
-    public void visit(PrimaryExpr pe);
+    public void visit(Expression x);
+    public void visit(AssignmentExpr x);
+    public void visit(BinaryExpr x);
+    public void visit(CastExpr x);
+    public void visit(UnaryExpr x);
+    public void visit(PostfixExpr x);
+    public void visit(PrimaryExpr x);
     
     /* Stmt */
-    public void visit(StmtList _sl);
-    public void visit(ExpressionStmt es);
-    public void visit(CompoundStmt cs);
-    public void visit(SelectionStmt ss);
-    public void visit(JumpStmt js);
-    public void visit(IterationStmt is);
+    public void visit(StmtList x);
+    public void visit(ExpressionStmt x);
+    public void visit(CompoundStmt x);
+    public void visit(SelectionStmt x);
+    public void visit(JumpStmt x);
+    public void visit(IterationStmt x);
     
     /* Decl */
-    public void visit(StarList _p);
-    public void visit(Declaration decl);
-    public void visit(FuncDeclarator fd);
-    public void visit(VarDeclarator vd);
-    public void visit(DeclarationList ds);
-    public void visit(DeclaratorList ds);
-    public void visit(InitDeclarator id);
-    public void visit(InitDeclaratorList ids);
-    public void visit(Initializer ini);
-    public void visit(InitializerList ir);
-    public void visit(NonInitDeclaration nid);
-    public void visit(NonInitDeclarationList nids);
-    public void visit(PlainDeclaration pd);
-    public void visit(PlainDeclarator pdr);
+    public void visit(StarList x);
+    public void visit(Declaration x);
+    public void visit(FuncDeclarator x);
+    public void visit(VarDeclarator x);
+    public void visit(DeclarationList x);
+    public void visit(DeclaratorList x);
+    public void visit(InitDeclarator x);
+    public void visit(InitDeclaratorList x);
+    public void visit(Initializer x);
+    public void visit(InitializerList x);
+    public void visit(NonInitDeclaration x);
+    public void visit(NonInitDeclarationList x);
+    public void visit(PlainDeclaration x);
+    public void visit(PlainDeclarator x);
 
     /* Func */
-    public void visit(FuncDef func);
-    public void visit(ArgumentList args);
-    public void visit(ParameterList pl);
+    public void visit(FuncDef x);
+    public void visit(ArgumentList x);
+    public void visit(ParameterList x);
 
     /* Type */
-    public void visit(TypeName t);
-    public void visit(TypeSpecifier ts);
+    public void visit(TypeName x);
+    public void visit(TypeSpecifier x);
     
     /* Program */
-    public void visit(Program prog);
+    public void visit(Program x);
 }
