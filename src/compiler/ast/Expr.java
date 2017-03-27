c8bdfbf5d305a2004cb59b204e9e76ea97b9d068
Yu Cang <yhcy1993@gmail.com>
2017-03-27 22:14:17 +0800

    add attributes

--- src/compiler/ast/Expr.java
+++ src/compiler/ast/Expr.java
@@ -1,6 +1,13 @@
 package compiler.ast;
 
+import compiler.semantic.Type;
+
 public abstract class Expr extends ASTNode
 {
+	public boolean isConst;
+	public Object value;
+	public boolean hasInitialized = false;
+	public Type type;
+	public boolean isLvalue = false;
 }
 
