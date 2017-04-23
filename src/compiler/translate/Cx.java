655b43522fbdef9696a7573efa15a6b309ef03be
CangYu <yhcy1993@gmail.com>
2017-04-23 18:24:46 +0800

    test space and tab

--- src/compiler/translate/Cx.java
+++ src/compiler/translate/Cx.java
@@ -12,29 +12,29 @@ import compiler.translate.temp.Temp;
 
 public abstract class Cx extends Exp
 {
-	@Override
-	public compiler.translate.ir.Exp unEx()
-	{
-		Temp r = new Temp();
-		Label t = new Label();
-		Label f = new Label();
-		
-		return new ESEQ(new SEQ(new MOVE(new TEMP(r),
-										 new CONST(1)),
-								new SEQ(unCx(t, f),
-										new SEQ(new LABEL(f),
-												new SEQ(new MOVE(new TEMP(r), 
-																 new CONST(0)),
-														new LABEL(t))))),
-						new TEMP(r));
-	}
+    @Override
+    public compiler.translate.ir.Exp unEx()
+    {
+        Temp r = new Temp();
+        Label t = new Label();
+        Label f = new Label();
 
-	@Override
-	public Stm unNx()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+        return new ESEQ(new SEQ(new MOVE(new TEMP(r), 
+                                         new CONST(1)),
+                                new SEQ(unCx(t, f), 
+                                        new SEQ(new LABEL(f), 
+                                                new SEQ(new MOVE(new TEMP(r), 
+                                                                 new CONST(0)), 
+                                                        new LABEL(t))))), 
+                        new TEMP(r));
+    }
 
-	public abstract Stm unCx(Label t, Label f);
+    @Override
+    public Stm unNx()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public abstract Stm unCx(Label t, Label f);
 }
