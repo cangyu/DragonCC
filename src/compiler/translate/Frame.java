7af30326bf4b33792b4d82218c143a1a91ae549f
CangYu <yhcy1993@gmail.com>
2017-04-28 17:33:57 +0800

    fix drop

--- src/compiler/translate/Frame.java
+++ src/compiler/translate/Frame.java
@@ -4,7 +4,6 @@ public abstract class Frame
 {
     public Label name;
     public AccessList formals = null;
-    public LinkedList
 
     public abstract Frame newFrame(Label name, BoolList formals);
     public abstract Access allocLocal(boolean escape);
